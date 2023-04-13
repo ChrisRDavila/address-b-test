38eddd85f93dbfb5ce15918841d5c580e2c5f3fc
ChrisRDavila <crossdavila@gmail.com>
2023-04-13 16:32:31 -0700

    WIP, break for day

--- __tests__/address-b-test.test.js
+++ __tests__/address-b-test.test.js
@@ -1,19 +1,28 @@
 import Contact from './../src/address-b-test.js';
 
 describe('Contact', () => {
+  // let contact;
+  // BeforeEach(( => {
 
-  test('should create new Contact Object with listed propertues', () => {
-    const newContact = new Contact("ross", "davila", "559-3333", "73 n way", "ross@ya");
+  // })
+
+  test('should create new Contact Object with listed properties, and split home and work emails', () => {
+    const newContact = new Contact("ross", "davila", "559-3333", "73 n way", "ross@ya", "ross@gm");
     expect(newContact.firstName).toEqual("ross");
     expect(newContact.lastName).toEqual("davila");
     expect(newContact.phoneNumber).toEqual("559-3333");
     expect(newContact.physicalAddress).toEqual("73 n way");
-    expect(newContact.emailAddress).toEqual("ross@ya");
+    expect(newContact.emailAddress).toEqual(["ross@ya", "ross@gm"]);
   });
-});
 
-  // this.firstName = firstName;
-  // this.lastName = lastName;
-  // this.phoneNumber = phoneNumber;
-  // this.physicalAddress = physicalAddress;
-  // this.emailAddress = [personalEmail, workEmail];
+
+  test("should combine first name with last name", () => {
+    const newContact = new Contact("ross", "davila", "559-3333", "73 n way", "ross@ya", "ross@gm");
+    // const newFullName = new Contact("ross", "davila");
+  newContact.fullName();
+  // expect(newFullName.prototype.fullName(firstName + " " + lastName)).toEqual("ross johnson");
+  // });
+    expect(newContact.fullName()).toEqual("ross davila");
+    });
+  }); 
+
--- src/address-b-test.js
+++ src/address-b-test.js
@@ -1,19 +1,11 @@
-export default function Contact(firstName, lastName, phoneNumber, physicalAddress, emailAddress) {
+export default function Contact(firstName, lastName, phoneNumber, physicalAddress, personalEmail, workEmail) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
   this.physicalAddress = physicalAddress;
-  this.emailAddress = emailAddress;
+  this.emailAddress = [personalEmail, workEmail]
 }
 
-// function Contact(firstName, lastName, phoneNumber, physicalAddress, personalEmail, workEmail) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-//   this.phoneNumber = phoneNumber;
-//   this.physicalAddress = physicalAddress;
-//   this.emailAddress = [personalEmail, workEmail]; 
-// }
-
-// Contact.prototype.fullName = function() {
-//   return this.firstName + " " + this.lastName;
-// };
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
